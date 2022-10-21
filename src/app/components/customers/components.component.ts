e089c2bf45239d8102c8d3b09e7679315381733e
Jeremias ramirez <Jereramirez499@gmail.com>
2022-10-21 06:11:32 -0400

    agregar y actualizar clientes agregado

--- src/app/components/customers/components.component.ts
+++ src/app/components/customers/components.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnChanges, OnInit } from '@angular/core'; 
+import { Component, OnChanges, OnInit } from '@angular/core';  
+import { ClientsService } from 'src/app/services/clients.service';
 import { CustomersService } from 'src/app/services/customers.service';
 import { LoginService } from 'src/app/services/login.service';
 import { SesionService } from 'src/app/services/sesion.service';
-
+ 
 @Component({
   selector: 'customers',
   templateUrl: './customers.component.html',
@@ -12,42 +13,109 @@ import { SesionService } from 'src/app/services/sesion.service';
   providers:[
     SesionService,
     LoginService,
-    CustomersService
+    CustomersService,
+    ClientsService
   ]
 })
 export class CustomersComponent implements OnInit {
 
   public formName:string = '';
   public formEmail:string = '';
+  public formEditName:string = '';
+  public formEditEmail:string = '';
+  public formEditId:number = 0;
+
+
   public dateStart:any = new Date().toString().substring(0,16);
-  
-  
+  public isFormValidated:any='';
+  public existContainerEdit:boolean=false;
+
+
+
+
   constructor(
+    public clientsService:ClientsService,
     public customerService:CustomersService,
     public loginService:LoginService,
     public sessionServices:SesionService) { }
     public isCharginData:boolean=true;
-    public customers:any[] = [];
+    public customers:customerI[] = [];
     
   ngOnInit (): void {
+
+
     this.sessionServices.verifiedSession('customers');
 
-    setTimeout(()=>{
-      this.customers= [
-        {id:1,name:'Jeremias ramirez', email:'jeremias@gmail.com',date:'tue 9 2022'},
-        {id:2,name:'Alexio m', email:'alexiom@gmail.com',date:'tue 22 2022'}
-      ]
-      this.isCharginData=false
-    },1000)
+
+
+    this.getAllCustomer();
+
+ 
+
+  }
+
+  public getAllCustomer(){
+    
+    
+    this.clientsService.getAllClients()
+    .subscribe((customer)=>{
+      this.customers=customer.response;  
+    });
+
+  }
+
+
+  public setNewCustomer(){
+     
+    if(this.formName.length>5 && this.formEmail.length>5){
+      this.isFormValidated='true';
+     
+      this.clientsService.setClientes(this.formName,this.formEmail,this.dateStart)
+      .subscribe((posted)=>{
+        this.getAllCustomer();
+        this.resetForm()
+      });
+
+    }else{
+      this.isFormValidated='false';
+    }
+
+   
+
+
+  }
+
+  public resetForm(){
+   this.formName=''
+   this.formEmail=''
   }
 
   public getCustomerByClick(customer:customerI){
-    console.log(customer)
+    this.existContainerEdit=true;
+
+
+    this.formEditName=customer.nombre;
+    this.formEditEmail=customer.email;
+    this.formEditId=customer.id;
+
+ 
   }
+
+  public updateCustomer(){
+    this.clientsService.updateClient(this.formEditId,this.formEditName,this.formEditEmail,this.dateStart)
+    .subscribe((e)=>{
+      this.getAllCustomer();
+      
+    })
+  }
+
+  
+
+
 
   public addCustomer(){
      
-    this.customers.splice(0,0,{id:2,name:'Alexio m', email:'alexiom@gmail.com',date:'fri 4 2021'})
+    this.setNewCustomer()
     
   }
   
@@ -58,7 +126,8 @@ export class CustomersComponent implements OnInit {
 
 interface customerI{
   id:number,
-  name:string,
+  nombre:string,
   email:string,
-  date:string
+  fechaRegistro:string,
+  idUsuario:number
 }
---
+++ src/app/services/clients.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core" 
+import { ajax } from 'rxjs/ajax';
+
+@Injectable()
+
+export class ClientsService {
+    
+      
+   constructor(){  }
+
+
+   public getAllClients(){
+        return ajax.get(`https://localhost:7279/clientes/all`)
+   }
+
+   public setClientes(name:string,email:string,date:string){
+        return ajax.post(`https://localhost:7279/clientes/new?name=${name}&email=${email}&date=${date}`)
+   }
+
+   public updateClient(id:number,name:string,email:string,date:string){
+     return ajax.post(`https://localhost:7279/clientes/update?id=${id}&name=${name}&email=${email}&date=${date}`)
+   }
+   
+
+
+}
